917a1066c827c496340e70c703eecbcffc8b7689
gkwlsdn17 <gkwlsdn17@gmail.com>
2021-10-14 20:30:06 +0900

    dotProduct

---
+++ DotProduct/DotProduct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DotProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("내적");
+            int[] num1 = { 1, 2, 3, 4 };
+            int[] num2 = { -3, -1, 0, 2 };
+            Console.WriteLine(dotProduct(num1, num2));
+        }
+
+        public static int dotProduct(int[] a, int[] b)
+        {
+            int sum = 0;
+            for(int i=0; i<a.Length; i++)
+            {
+                sum += a[i] * b[i];
+            }
+            return sum;
+        }
+    }
+}
