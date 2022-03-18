71a1ef5d6b9ce5128181fcd26256a679bcb57bde
gkwlsdn17 <gkwlsdn17@gmail.com>
2022-03-18 22:41:14 +0900

    순열
    c#순열

---
+++ Permutation/Permutation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = { 5, 2, 3, 4, 1 };
+            Perm(arr, 0, arr.Length, arr.Length);
+        }
+
+        static void Perm(int[] arr, int depth, int n, int k)
+        {
+            if(depth == k)
+            {
+                for(int i=0; i < k; i++)
+                {
+                    if (i == k - 1)
+                        Console.WriteLine(arr[i]);
+                    else
+                        Console.Write(arr[i] + ",");
+                    
+                }
+                
+                return;
+            }
+            
+            for(int i=depth; i < n; i++)
+            {
+                swap(arr, i, depth);
+                Perm(arr, depth + 1, n, k);
+                swap(arr, i, depth);
+            }
+        }
+
+        static void swap(int[] arr, int i, int j)
+        {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
